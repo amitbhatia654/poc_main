c815177920fb65ea60843d0a127ea1e5abc3cb48
Amit Bhatia <amitbhatia654@gmail.com>
2025-01-31 09:26:07 +0530

    changes in webpack config file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import EmailComp from 'app1/App'
+import ChatComp from 'app2/App'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h2> My Main App </h2>
+      <EmailComp></EmailComp>
+      <ChatComp></ChatComp>
     </div>
   );
 }
